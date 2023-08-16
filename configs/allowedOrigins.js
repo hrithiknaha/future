876430dc17aa1519d1ac71cd4a04d2c6e6dda0f7
Hrithik Naha <naha.hrithkk@gmail.com>
2023-08-16 19:06:41 +0530

    add client to allowedOrigins

--- configs/allowedOrigins.js
+++ configs/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ["http://localhost:3000", "http://192.168.1.11:3000"];
+const allowedOrigins = ["http://localhost:3000", "http://192.168.1.11:3000", "https://future-archive.onrender.com"];
 
 module.exports = allowedOrigins;
